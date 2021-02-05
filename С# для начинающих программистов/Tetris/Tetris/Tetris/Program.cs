cbae8bb0a79b02fe1dfc8e63eac02b9306f4d4a4
Stas <stanislav.parfilov@ics-it.ru>
2021-02-06 01:33:19 +0300

    prog

---
+++ С# для начинающих программистов/Tetris/Tetris/Tetris/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            char z = '*';
+
+            Figure s = new Stick(20, 5, z);
+            s.Draw();
+
+            Thread.Sleep(500);
+            s.Hide();
+            s.Rotate();
+            s.Draw();
+
+            Thread.Sleep(500);
+            s.Hide();
+            s.Rotate();
+            s.Draw();
+
+            Thread.Sleep(500);
+            s.Hide();
+            s.Rotate();
+            s.Draw();
+
+            Thread.Sleep(500);
+            s.Hide();
+            s.Rotate();
+            s.Draw();
+
+
+            //s.Move(Direction.LEFT);
+
+
+            //Figure[] f = new Figure[2];
+            //f[0] = new Square(2, 5, z);
+            //f[1] = new Stick(6, 6, z);
+
+            //foreach (Figure fig in f)
+            //{
+            //    fig.Draw();
+            //}
+
+            Console.ReadLine();
+        }
+
+    }
+}
