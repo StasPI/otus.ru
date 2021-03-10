a731ddad046cd9db6ae444b010e4ec8347559794
stanislav.parfilov <stanislav.parfilov@ics-it.ru>
2021-03-11 02:01:41 +0300

    gg

--- С# для начинающих программистов/TetrisSb/TetrisSb/TetrisSb/GuiDrawer.cs
+++ С# для начинающих программистов/TetrisSb/TetrisSb/TetrisSb/GuiDrawer.cs
@@ -35,7 +35,7 @@ namespace TetrisSb
       {
          GraphicsWindow.BrushColor = "Red";
          GraphicsWindow.FontSize = 20;
-         GraphicsWindow.DrawText((Field.Width / 2 - 4) * SIZE, (Field.Height / 2) * SIZE, "GAME OVER");
+         GraphicsWindow.DrawText((Field.Width / 2 - 4) * SIZE, (Field.Height / 2) * SIZE, "G A M E   O V E R");
       }
    }
 }
